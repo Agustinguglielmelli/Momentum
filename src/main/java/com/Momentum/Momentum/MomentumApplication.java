63ac7ab64577606024acaac6446d81f9f621ba9c
Agustín Guglielmelli <aguglielmelli@mail.austral.edu.ar>
2025-03-28 14:59:24 -0300

    setup

---
+++ src/main/java/com/Momentum/Momentum/MomentumApplication.java
@@ -0,0 +1,13 @@
+package com.Momentum.Momentum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentumApplication.class, args);
+	}
+
+}
