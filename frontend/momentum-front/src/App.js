48d4b7bde293af88da6be661b701787ed99f5d69
Agustín Guglielmelli <aguglielmelli@mail.austral.edu.ar>
2025-03-31 15:22:24 -0300

    table

--- frontend/momentum-front/src/App.js
+++ frontend/momentum-front/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Table from "./components/table/Table";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hola</h1>
+        <Table />
       </header>
     </div>
   );
