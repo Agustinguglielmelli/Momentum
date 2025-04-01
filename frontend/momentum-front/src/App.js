a1bc517d829f24cb630aaf3cc815f7a5fc7eb17a
EnzoBersano <bersanoenzo@gmail.com>
2025-04-01 09:31:08 -0300

    agrego el componente de registro usuario a adduser y lo pruebo

--- frontend/momentum-front/src/App.js
+++ frontend/momentum-front/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from "./components/table/Table";
+import Registrousuario from "./components/registrousuario/Registrousuario";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Table />
+        <Registrousuario />
       </header>
     </div>
   );
---
+++ frontend/momentum-front/src/pages/AddUser.js
@@ -0,0 +1,16 @@
+
+
+//podriamos usar el componente de registro de usuario y agregar este boton para poder elegir el rol de usuario
+import React from 'react';
+import { useState } from 'react';
+import Registrousuario from '../components/registrousuario/Registrousuario';
+
+function AddUser(){
+    return(
+        <Registrousuario />
+    )
+    
+    }
+    
+export default AddUser;
+
