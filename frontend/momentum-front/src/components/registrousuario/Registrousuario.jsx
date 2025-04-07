c5ff05b6f9aad30db9107a6d0cb8408db77eeb7a
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-04-07 16:41:18 -0300

    button back

--- frontend/momentum-front/src/components/registrousuario/Registrousuario.jsx
+++ frontend/momentum-front/src/components/registrousuario/Registrousuario.jsx
@@ -1,6 +1,7 @@
 import Button from "../button/Button";
 import axios from "axios";
-import React, {useState} from "react"; //esta bien importado cuando es "../button/Button"
+import React, {useState} from "react";
+import {Link} from "react-router-dom"; //esta bien importado cuando es "../button/Button"
 
 
 function Registrousuario() {
@@ -79,77 +80,79 @@ function Registrousuario() {
     const [base64, setBase64] = useState('');
 
 
-    return (<div className="w-50 mx-auto border p-5 shadow bg-body-secondary border-light-secondary rounded-lg">
-        <h1>Registro de Usuario</h1>
-        <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-                <label htmlFor="username" className="form-label">Username: </label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setusername(e.target.value)} // e.target.value es el contenido del input que se escribe
-                    required // Hay que completar el campor para poder darle submit
-                />
-            </div>
+    return (
+    <div >
+        <Link to='/' className="btn btn-primary">Back</Link>
+        <div className="w-50 mx-auto border p-5 shadow bg-body-secondary border-light-secondary rounded-lg">
+            <h1>Registro de Usuario</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Username: </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setusername(e.target.value)} // e.target.value es el contenido del input que se escribe
+                        required // Hay que completar el campor para poder darle submit
+                    />
+                </div>
 
-            <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email address: </label>
-                <input type="email"
-                       className="form-control"
-                       id="email"
-                       aria-describedby="emailHelp"
-                       value={email}
-                       onChange={(e) => setemail(e.target.value)}
-                       required
-                />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-            </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address: </label>
+                    <input type="email"
+                           className="form-control"
+                           id="email"
+                           aria-describedby="emailHelp"
+                           value={email}
+                           onChange={(e) => setemail(e.target.value)}
+                           required
+                    />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
 
-            <div className="mb-3">
-                <label htmlFor="password" className="form-label">Password: </label>
-                <input type="password"
-                       className="form-control"
-                       id="password"
-                       value={password}
-                       onChange={(e) => setpassword(e.target.value)}
-                        required
-                />
-            </div>
-{
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password: </label>
+                    <input type="password"
+                           className="form-control"
+                           id="password"
+                           value={password}
+                           onChange={(e) => setpassword(e.target.value)}
+                            required
+                    />
+                </div>
 
-            <div className="mb-3">
-                <label htmlFor="profilePicture">Profile Picture:</label>
-                <input type="file" id="profilePicture" name="profilePicture" accept="image/*" onChange={handleFileChange}/>
-                {base64 && (
-                    <div>
-                        <p>Preview:</p>
-                        <img src={base64} alt="preview" style={{ maxWidth: '300px' }} />
-                    </div>
-                )}
 
-            </div>
+                <div className="mb-3">
+                    <label htmlFor="profilePicture">Profile Picture:</label>
+                    <input type="file" id="profilePicture" name="profilePicture" accept="image/*" onChange={handleFileChange}/>
+                    {base64 && (
+                        <div>
+                            <p>Preview:</p>
+                            <img src={base64} alt="preview" style={{ maxWidth: '300px' }} />
+                        </div>
+                    )}
 
-  /*          <div className="mb-3">
-                <input
-                    type="radio"
-                    name="role"
-                    value="RUNNER"
-                    onChange={(e) => setRole(e.target.value)}
-                /> Runner
-                <input type="radio"
-                       name="role"
-                       value="COACH"
+                </div>
+
+               {/*<div className="mb-3">
+                    <input
+                        type="radio"
+                        name="role"
+                        value="RUNNER"
                         onChange={(e) => setRole(e.target.value)}
-                /> Coach
-            </div>
-*/
-}
+                    /> Runner
+                    <input type="radio"
+                           name="role"
+                           value="COACH"
+                            onChange={(e) => setRole(e.target.value)}
+                    /> Coach
+                </div>*/}
 
 
-            <Button type="submit" className="btn-primary" text="Submit"></Button>
-        </form>
+                <Button type="submit" className="btn-primary" text="Submit"></Button>
+            </form>
+        </div>
     </div>);
 }
 
