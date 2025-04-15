8acb5b87d48e938d49870134cf8dbb414a354463
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-04-15 09:34:09 -0300

    navbar

--- frontend/momentum-front/src/components/navbar/Navbar.jsx
+++ frontend/momentum-front/src/components/navbar/Navbar.jsx
@@ -1,12 +1,26 @@
 import { Link } from 'react-router-dom';
-
+import './Navbar.css'
 function Navbar() {
     return (
-        <nav className='menu'>
-            <Link to='/miperfil'>Mi Perfil</Link>
-            <Link to='feed'>Feed</Link>
-            <Link to='/eventos'>Eventos</Link>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page"  to='/miperfil/createTrainingPlan'>My profile</Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Feed</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Events</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
         </nav>
     )
 }
-export default Navbar; 
+
+export default Navbar;
