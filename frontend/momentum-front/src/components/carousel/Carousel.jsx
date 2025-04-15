6c60af5bd8b823089e4984b2f7d3ff2be6c53fb2
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-04-15 20:07:06 -0300

    funciona carousel (no tiene animacion)

--- frontend/momentum-front/src/components/carousel/Carousel.jsx
+++ frontend/momentum-front/src/components/carousel/Carousel.jsx
@@ -1,8 +1,26 @@
+import {useState} from "react";
 
 
 function Carousel({ imageList }) {
+    const [imageIndex, setImageIndex] = useState(0);
+
     if (!Array.isArray(imageList) || imageList.length === 0) {
         return <div>No images available</div>;  // Muestra un mensaje si no hay imágenes
+    }
+
+
+    function handleNext(){
+        /*Le paso el indice, si el indice es igual a length de la lista
+        * significa que estoy en la ultima foto y debo volver a la primera,
+        * entonces seteo 0, sino, seteo prevIndex + 1 */
+        setImageIndex((prevIndex) =>
+            prevIndex === imageList.length - 1 ? 0 : prevIndex + 1
+        );
+    }
+    function handlePrev(){
+        setImageIndex((prevIndex) =>
+            prevIndex === 0 ? imageList.length - 1 : prevIndex - 1
+        );
     }
     return (
 
@@ -11,7 +29,8 @@ function Carousel({ imageList }) {
                 {imageList.map((image, index) => (
                     <div
                         key={index}
-                        className={`carousel-item ${index === 0 ? "active" : ""}`} // solo la imagen con index 0 es active
+                        /* solo la imagen con index 0 es active */
+                        className={`carousel-item ${index === imageIndex ? "active" : ""}`}
                     >
                         <img
                             className="d-block w-100"
@@ -22,12 +41,15 @@ function Carousel({ imageList }) {
                 ))}
             </div>
 
-            {/* los botones estos andan porque importamos en index.js un script de bootstrap */}
-            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <a className="carousel-control-prev" href="#carouselExampleControls"
+               role="button" data-bs-slide="prev"
+            onClick={handlePrev}>
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="sr-only">Previous</span>
             </a>
-            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <a className="carousel-control-next" href="#carouselExampleControls"
+               role="button" data-bs-slide="next"
+            onClick={handleNext}>
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="sr-only">Next</span>
             </a>
