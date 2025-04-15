6751f9428474cb95214dc499f39135a7e8e8a86f
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-04-15 11:09:06 -0300

    .

--- frontend/momentum-front/src/components/post/recreationalpost/RecreationalPost.jsx
+++ frontend/momentum-front/src/components/post/recreationalpost/RecreationalPost.jsx
@@ -1,10 +1,10 @@
+import { useState } from "react";
+export function RecreationalPost() {
 
-export function RecreationalPost( {} ) {
-
-    const [recreationalPosts, setRecreationalPosts] = React.useState([]);
+    const [recreationalPosts, setRecreationalPosts] = useState([]);
 
     return (
-        <div className="card" style="width: 18rem;">
+        <div className="card">
             <img src="." className="card-img-top" alt=""/>
             <div className="card-body">
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
--- frontend/momentum-front/src/components/router/AppRouter.jsx
+++ frontend/momentum-front/src/components/router/AppRouter.jsx
@@ -8,20 +8,26 @@ import SignupUser from "../signupuser/SignupUser";
 import RecreationalPostForm from "../post/recreationalpostform/RecreationalPostForm";
 import TrainingPlanPostForm from "../post/trainingplanpostform/TrainingPlanPostForm";
 import Navbar from "../navbar/Navbar";
+import {RecreationalPost} from "../post/recreationalpost/RecreationalPost";
 
 function AppRouter() {
     return (
         <>
         <Routes> 
 
-
             <Route path='/' element={<SignupUser />} />
+            <Route path='/signup' element={<SignupUser />} />
             <Route path='/login' element={<LoginUser/>}></Route>
             <Route path='/home' element={<Navbar/>}></Route>
 
             <Route path='/miperfil/createTrainingPlan' element={
                 <PrivateRoute>
                     <TrainingPlanPostForm />
+                </PrivateRoute>
+            } />
+            <Route path='/miperfil/recreationalPost' element={
+                <PrivateRoute>
+                    <RecreationalPost/>
                 </PrivateRoute>
             } />
             <Route path='/miperfil/createRecreationalPost' element={
