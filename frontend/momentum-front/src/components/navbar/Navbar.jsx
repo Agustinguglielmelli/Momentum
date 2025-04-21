8fbd0cfbc97054650225e8f5b0a0b8406c138339
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-04-21 20:02:30 -0300

    agrego otra opcion de navbar

--- frontend/momentum-front/src/components/navbar/Navbar.jsx
+++ frontend/momentum-front/src/components/navbar/Navbar.jsx
@@ -2,14 +2,20 @@ import { Link } from 'react-router-dom';
 import './Navbar.css'
 function Navbar() {
     return (
-    <div className="navbar">
-      <Link to="/miperfil" >My Profile</Link>
-      <Link to="/home" >Feed</Link>
-      <Link to="/events">Events</Link>
-      
-    </div>
+        <div>
+            <div className="navbar">
+                <Link className="navbar-a" to="/miperfil">My Profile</Link>
+                <Link className="navbar-a" to="/home">Feed</Link>
+                <Link className="navbar-a" to="/events">Events</Link>
+            </div>
+            <nav className="navbar2">
+                <a href="#" className="navbar-link">My profile</a>
+                <a href="#" className="navbar-link">Feed</a>
+                <a href="#" className="navbar-link">Events</a>
+            </nav>
+        </div>
     )
-        
+
 
 }
 
